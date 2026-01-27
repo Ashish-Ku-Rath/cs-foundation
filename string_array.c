542a7b0e681987bd7fb992b8cc747f4458bb42d9
Ashish-Kumar-Rath <ashishkumarrath00@gmail.com>
2026-01-27 23:35:22 +0530

    Working day 10-String with pointer basics

---
+++ string_array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    char str[50];
+    printf("enter your string:\n");
+    fgets(str,50,stdin);
+    for(int i=0; str[i]!='\0';i++){
+        printf("%c",str[i]);
+    }
+    return 0;
+}
---
+++ string_pointer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    char str[50];
+    char *ptr ;
+    ptr = str ;
+    printf("enter your string:\n");
+    fgets(str,50,stdin);
+    for(int i=0; *(ptr+i)!='\0';i++){
+        printf("%c",*(ptr+i));
+    }
+    return 0;
+}
